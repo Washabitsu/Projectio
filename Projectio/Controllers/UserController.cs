456d31acd47cd1dac3061cc3e824663a9a44e470
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-16 14:47:49 +0200

    Refactor UserController for RoleManager and UserManager Usage
    refactor: use RoleManager and UserManager for all role and user operations in UserController

--- Projectio/Controllers/UserController.cs
+++ Projectio/Controllers/UserController.cs
@@ -1,18 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Projectio.Core.Dtos;
+using Projectio.Core.Interfaces;
 using Projectio.Core.Models;
 using Projectio.Persistence;
-using System.Security.Cryptography;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.IdentityModel.Tokens.Jwt;
-using Projectio.Helpers;
-using Projectio.Core.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Projectio.Controllers
@@ -22,16 +16,18 @@ namespace Projectio.Controllers
     public class UserController : ControllerBase
     {
         private IJWT _jwt { get; set; }
-        private IMapperWrapper _mapperWrapper { get; set; }
+        private IMapper _mapper { get; set; }
         private ApplicationDbContext _context { get; set; }
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         private readonly UserManager<ApplicationUser> _userManager;
-        public UserController(ApplicationDbContext context, IJWT jwt, IMapperWrapper mapperWrapper, UserManager<ApplicationUser> userManager)
+        public UserController(ApplicationDbContext context, IJWT jwt, IMapper mapper, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
-            _mapperWrapper = mapperWrapper;
+            _mapper = mapper;
             _jwt = jwt;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
 
@@ -49,16 +45,13 @@ namespace Projectio.Controllers
             if (user == null)
                 return NotFound("User not found!");
 
-            var dto = _mapperWrapper.Map<ApplicationUser, UserOutDTO>(user);
-            var role_id = (await _context.UserRoles.FirstOrDefaultAsync(ru => ru.UserId == user.Id)).RoleId;
-            if (role_id != null)
-            {
-                var role = await _context.Roles.FirstOrDefaultAsync(r => r.Id == role_id);
-                if (role != null)
-                {
-                    dto.Role = role.Name;
-                }
-            }
+            var dto = _mapper.Map<ApplicationUser, UserOutDTO>(user);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+                dto.Role.Append(role);
+            
 
             return Ok(dto);
         }
@@ -77,18 +70,11 @@ namespace Projectio.Controllers
            
             try
             {
-                IdentityRole new_role = null;
-                if (value.RoleId != null)
-                {
-                    new_role = await _context.Roles.Where(r => r.Id == value.RoleId).FirstAsync();
-                }
-                else if (value.Role != null)
-                {
-                    new_role = await _context.Roles.Where(r => r.NormalizedName == value.Role.ToUpper()).FirstAsync();
-                }
-
-
+                IdentityRole new_role = await _roleManager.FindByNameAsync(value.Role);
+    
+              
                 var result = await _userManager.CreateAsync(user, value.Password);
+
                 if (!result.Succeeded)
                     return BadRequest(result.Errors);
 
@@ -111,23 +97,15 @@ namespace Projectio.Controllers
             Request.Headers.TryGetValue("Authorization", out var token);
             ApplicationUser user;
             var username = await _jwt.GetRoleFJTW(token);
-            var role = await _jwt.GetRoleFJTW(token);
-            if (value.UserId != null)
-            {
-                if (role == "Admin")
-                    user = (await _context.Users.FindAsync(value.UserId));
-                else
-                    return Unauthorized("You are not authorized!");
-            }
-            else
-            {
-                if (username == null)
-                    return NotFound("Username not found!");
-                user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == username);
-            }
+            user = await _context.Users.FirstOrDefaultAsync(user => user.UserName == username);
 
             if (user == null)
                 return NotFound("User not found!");
+
+            if (!await _userManager.CheckPasswordAsync(user,value.CurrentPassword))
+            {
+                return Unauthorized("You are not authorized!");
+            }
 
             user.UpdateUser(value);
 
@@ -139,47 +117,37 @@ namespace Projectio.Controllers
                 var passwordResult = await _userManager.ChangePasswordAsync(user, value.CurrentPassword, value.Password);
             }
 
-            if (value.RoleId != null)
+            if (value.Role != null)
             {
-                var new_role = await _context.Roles.FirstAsync(r => r.Id == value.RoleId);
-                var existing_roles = (from ur in _context.UserRoles join r in _context.Roles on ur.RoleId equals r.Id where ur.UserId == user.Id select r.Name).ToList();
-                foreach (var existing_role in existing_roles)
-                    await _userManager.RemoveFromRoleAsync(user, existing_role);
+                var new_role = await _roleManager.FindByNameAsync(value.Role);
+                var existing_roles = await _userManager.GetRolesAsync(user);
+                await _userManager.RemoveFromRolesAsync(user, existing_roles);
                 await _userManager.AddToRoleAsync(user, new_role.Name);
             }
+
             _context.Users.Update(user);
             await _context.SaveChangesAsync();
             return Ok("User has been updated!");
         }
 
         [HttpDelete]
-        public async Task<IActionResult> Delete(string id)
+        public async Task<IActionResult> Delete()
         {
             try
             {
                 Request.Headers.TryGetValue("Authorization", out var token);
                 ApplicationUser user;
                 var username = await _jwt.GetRoleFJTW(token);
-                var role = await _jwt.GetRoleFJTW(token);
-                if (id != null)
-                {
-                    if (role == "Admin")
-                        user = (await _context.Users.FindAsync(id));
-                    else
-                        return Unauthorized("You are not authorized!");
-                }
-                else
-                {
-                    if (username == null)
-                        return NotFound("Username not found!");
-                    user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == username);
-                }
+                user = await _context.Users.FirstOrDefaultAsync(user => user.UserName == username);
 
-                if (user == null)
+                if (user == null) {
                     return NotFound("User not found!");
+                }
 
                 _context.Users.Remove(user);
+
                 await _context.SaveChangesAsync();
+
                 return Ok("User has been deleted!");
             }
             catch (Exception ex)
