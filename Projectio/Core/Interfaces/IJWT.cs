55b653754e8fd05e5bdc5c269fa1cf9ae9e793ac
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-17 08:18:51 +0200

    Refactor JWT interface and add JWT provider
    Changed IJWT.GetJwtToken to return a string instead of JwtSecurityToken. Introduced IJWTProvider interface for RSA key management and implemented it in AppSettingsJwtProvider.

--- Projectio/Core/Interfaces/IJWT.cs
+++ Projectio/Core/Interfaces/IJWT.cs
@@ -6,7 +6,7 @@ namespace Projectio.Core.Interfaces
 {
     public interface IJWT
     {
-        Task<JwtSecurityToken> GetJwtToken(string username, List<Claim> additionalClaims = null);
+        Task<string> GetJwtToken(string username, List<Claim> additionalClaims = null);
         Task<string?> GetUsernameFJTW(StringValues bearer_token);
         Task<string?> GetRoleFJTW(StringValues bearer_token);
     }
