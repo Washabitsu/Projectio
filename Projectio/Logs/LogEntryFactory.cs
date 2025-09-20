962cbe6b4aac7bb76a416182de8cdabf4edf83ac
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-20 13:31:22 +0200

    Add ILogEntryFactory interface and implementation stub
    Introduces the ILogEntryFactory interface and a LogEntryFactory class with method stubs for creating various log entries. Methods are not yet implemented and currently throw NotImplementedException.

---
+++ Projectio/Logs/LogEntryFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Abstractions;
+using Projectio.Core.Interfaces;
+
+namespace Projectio.Logs
+{
+    public class LogEntryFactory : ILogEntryFactory
+    {
+        public LogEntry Create(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry CreateForbiddenLog(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry CreateLockedOutLog(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry CreateTokenMissMatchLog(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry CreateTokenValidationLog(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry CreateUnauthorizedLog(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
