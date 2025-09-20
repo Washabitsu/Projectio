de3ae0657813d6d38b6d41d38b9cc66a3d839bb5
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-20 13:30:59 +0200

    Add custom exception classes for authentication
    Introduced ForbiddenException, LockedOutException, TokenMissMatchException, TokenValidationException, and UnauthorizedAccess exception classes in the Projectio.Exceptions namespace to handle various authentication and authorization error scenarios.

---
+++ Projectio/Exceptions/TokenMissMatchException.cs
@@ -0,0 +1,13 @@
+﻿namespace Projectio.Exceptions
+{
+    public class TokenMissMatchException : Exception
+    {
+        /* Since I trust the token to be valid if it was signed with the correct private key in the case
+         * that the token contains a username that does not exist in the database I will throw this exception.
+         * This could be a IOC that the attacker is trying to use a token that was signed with the correct private key
+         * To impersonate a user that does not exist in the database or even enumerate users.
+        */
+        public TokenMissMatchException() : base("The provided token does not match the expected token.")
+        { }
+    }
+}
