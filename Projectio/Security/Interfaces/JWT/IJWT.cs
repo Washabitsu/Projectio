af79bd5aca775f977c302f217881aa8836be9241
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-23 15:43:28 +0200

    Refactor JWT interfaces and add configuration interface
    Moved IJWT interface from Core to Security namespace, updated method names and signatures for clarity, and added a new IJWTConfiguration interface to encapsulate JWT configuration settings.

---
+++ Projectio/Security/Interfaces/JWT/IJWT.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Primitives;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Projectio.Security.Interfaces.JWT
+{
+    public interface IJWT
+    {
+        Task<string> SignJwtToken(string username, List<Claim> additionalClaims = null);
+        Task<string?> GetUsernameFromToken(StringValues bearer_token);
+        Task<ClaimsPrincipal?> ValidateToken(StringValues bearer_token);
+        Task<IEnumerable<string?>> GetRolesFromToken(StringValues bearer_token);
+    }
+}
