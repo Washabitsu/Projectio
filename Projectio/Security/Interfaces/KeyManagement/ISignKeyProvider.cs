4f6ed8c7bb2d9ed9dfe8db51e5193d5e147fa6cc
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-23 15:42:23 +0200

    Refactor JWT key provider interfaces and add base class
    Removed the old IJWTProvider interface and introduced a new IEncryptionProvider interface under Security/Interfaces/KeyManagement with async methods for credentials and key retrieval. Added an abstract SignKeyProviderBase class implementing the new interface to support key management refactoring.

---
+++ Projectio/Security/Interfaces/KeyManagement/ISignKeyProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Projectio.Security.Interfaces.KeyManagement
+{
+    //Interface for JWT Provider to get RSA keys.
+    public interface IEncryptionProvider
+    {
+        Task<SigningCredentials> GetCredentials();
+        Task<SecurityKey> GetPublicKey();
+        Task<JsonWebKey> GetJWKS();
+
+
+    }
+}
---
+++ Projectio/Security/KeyManagement/SignKeyProviderBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Projectio.Security.Interfaces.KeyManagement;
+
+namespace Projectio.Security.KeyManagement
+{
+    public abstract class SignKeyProviderBase : IEncryptionProvider
+    {
+        protected byte[]? _privateKeyBytes;
+        public SignKeyProviderBase() { }
+
+        public abstract Task<SigningCredentials> GetCredentials();
+
+        public abstract Task<JsonWebKey> GetJWKS();
+
+        public abstract Task<SecurityKey> GetPublicKey();
+    }
+}
