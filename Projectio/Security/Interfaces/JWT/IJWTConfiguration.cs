2f4af55daf85980fd54b90bc6af29e6fe5cb8849
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-23 15:43:28 +0200

    Refactor JWT interfaces and add configuration interface
    Moved IJWT interface from Core to Security namespace, updated method names and signatures for clarity, and added a new IJWTConfiguration interface to encapsulate JWT configuration settings.

---
+++ Projectio/Security/Interfaces/JWT/IJWTConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Projectio.Security.Interfaces.JWT
+{
+    public interface IJWTConfiguration
+    {
+        string Issuer { get; set; }
+        string Audience { get; set; }    
+        string SigningKey { get; set; }
+        int TokenTimeoutMinutes { get; set; }
+    }
+}
