4999b28fbf6bff6f8cfc3943c7388af41fa12135
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-23 15:42:48 +0200

    Replace AppSettingsJwtProvider with ASKeyProvider
    Removed the AppSettingsJwtProvider class and introduced ASKeyProvider in the KeyManagement/SignKeyProviders namespace. The new provider leverages an ISigner interface for signing and key retrieval, improving modularity and aligning with updated security interfaces.

---
+++ Projectio/Security/KeyManagement/SignKeyProviders/ASKeyProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Projectio.Security.Interfaces;
+using Projectio.Security.Interfaces.KeyManagement;
+using Projectio.Security.Interfaces.Signing;
+using System.Security.Cryptography;
+
+namespace Projectio.Security.KeyManagement.SKProviders
+{
+    public class ASKeyProvider : SignKeyProviderBase
+    {
+        private readonly string _pk;
+        private readonly ISigner _signer;
+        public ASKeyProvider(ISigner signer,string privateKeyBase64)
+        {
+            _pk = privateKeyBase64;
+            _signer = signer;
+        }
+
+        public override async Task<SigningCredentials> GetCredentials() =>  await _signer.SignAsync(_pk);
+
+        public override async  Task<SecurityKey> GetPublicKey() => await _signer.RetrievePublicKey(_pk);     
+        
+        public override async Task<JsonWebKey> GetJWKS() => await _signer.GetJWK(_pk);
+
+       
+    }
+}
