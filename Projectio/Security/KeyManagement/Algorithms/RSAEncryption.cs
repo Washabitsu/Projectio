ea374bf8440fcac4075abf769d90234975975598
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-23 15:41:43 +0200

    Add ISigner interface and RSAEncryption implementation
    Introduces the ISigner interface for signing operations and provides an RSAEncryption class implementing this interface. The implementation includes methods for signing, retrieving public keys, and generating JWKs using RSA keys.

---
+++ Projectio/Security/KeyManagement/Algorithms/RSAEncryption.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Projectio.Security.Interfaces.KeyManagement;
+using Projectio.Security.Interfaces.Signing;
+using System.Security.Cryptography;
+
+
+namespace Projectio.Security.KeyManagement.Algorithms
+{
+    public class RSAEncryption : ISigner
+    {
+        public async Task<JsonWebKey> GetJWK(string privateKey)
+        {
+            var rsaKey = (RsaSecurityKey)await RetrievePublicKey(privateKey);
+            var rsa = rsaKey.Rsa!;
+            var parameters = rsa.ExportParameters(false);
+
+            var jwk = new JsonWebKey
+            {
+                Kty = JsonWebAlgorithmsKeyTypes.RSA,                   // "RSA"
+                N = Base64UrlEncoder.Encode(parameters.Modulus),     // modulus
+                E = Base64UrlEncoder.Encode(parameters.Exponent),    // exponent
+                Alg = SecurityAlgorithms.RsaSha256,                    // "RS256"
+                Use = JsonWebKeyUseNames.Sig,                          // "sig"
+
+            };
+
+            jwk.Kid = Base64UrlEncoder.Encode(jwk.ComputeJwkThumbprint());
+            return jwk;
+        }
+        
+
+        public async Task<SecurityKey> RetrievePublicKey(string privateKey)
+        {
+            var rsa = RSA.Create();
+            var private_bytes = Convert.FromBase64String(privateKey);
+            rsa.ImportPkcs8PrivateKey(private_bytes, out _);
+            byte[] publicKeyBytes = rsa.ExportSubjectPublicKeyInfo();
+            rsa = RSA.Create();
+            rsa.ImportSubjectPublicKeyInfo(publicKeyBytes, out _);
+            return new RsaSecurityKey(rsa);
+        }
+
+        public async Task<SigningCredentials> SignAsync(string privateKey)
+        {
+            var rsa = RSA.Create();
+            var keyBytes = Convert.FromBase64String(privateKey);
+            rsa.ImportPkcs8PrivateKey(keyBytes, out _);
+            return new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
+        }
+
+
+    }
+}
