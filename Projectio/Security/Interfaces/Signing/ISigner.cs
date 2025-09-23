787be392f0746465a787610a9871029c4202896c
Dimitris Argyropoulos <dimitris.argyropoulos@outlook.com>
2025-09-23 15:41:43 +0200

    Add ISigner interface and RSAEncryption implementation
    Introduces the ISigner interface for signing operations and provides an RSAEncryption class implementing this interface. The implementation includes methods for signing, retrieving public keys, and generating JWKs using RSA keys.

---
+++ Projectio/Security/Interfaces/Signing/ISigner.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Projectio.Security.Interfaces.Signing
+{
+    public interface ISigner
+    {
+       
+        public Task<SigningCredentials> SignAsync(string privateKey);
+        public Task<SecurityKey> RetrievePublicKey(string privateKey);
+        public Task<JsonWebKey> GetJWK(string privateKey);
+    }
+}
